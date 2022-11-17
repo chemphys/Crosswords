f3b5f4dd2573a7716a49aeee57944f447003f9ad
Marc <mrierari@ucsd.edu>
2022-11-17 11:58:51 -0500

    Documented word.h

--- src/word.h
+++ src/word.h
@@ -7,23 +7,33 @@
 #include <string>
 #include <iomanip>
 
+// Word class stores a word and useful information of that word
 class Word {
    public:
+
+    // Default constructor
     Word() {
         w_ = "";
         orientation_ = 0;
         availability_.clear();
         anchor_.clear();
     }
-
+    
+    // Overloaded constructor.
+    // w: string with the word
+    // orientation: either ifthe word is vertical (top to bottom)
+    //              or horizontal (left to right)
     Word(std::string w, int orientation) {
         w_ = w;
         orientation_ = orientation;
         availability_ = std::vector<int>(w.size() + 2, 0);
     }
 
+    // Default destructor. No extra work needed
     ~Word() {}
 
+    // Overloaded == operator. Two words are equal if the orientation, availability,
+    // and actual word are the same. Anchor does not matter.
     bool operator==(const Word w) {
         bool out = true;
         out = out && orientation_ == w.orientation_;
@@ -32,6 +42,7 @@ class Word {
         return out;
     }
 
+    // Useful tool to print the information on the standard output.
     void Print() {
         std::cout << w_ << " ; orientation = " << orientation_ << std::endl;
         for (size_t i = 0; i < availability_.size(); i++) {
@@ -40,6 +51,7 @@ class Word {
         std::cout << std::endl;
     }
 
+    // Quick way to access the number of letters of the word
     int size() { return w_.size(); }
 
     // The actual word
@@ -53,7 +65,7 @@ class Word {
     // 0 is available, 1 is next to intersection, 2 is intersection with another word
     std::vector<int> availability_;
 
-    // The ancor in space
+    // The ancor in space (assumed a 2d grid)
     std::vector<int> anchor_;
 };
 
