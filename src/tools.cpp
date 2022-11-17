393bd04f14dc47c067bb6abc5602e66d3413cab0
Marc <mrierari@ucsd.edu>
2022-11-17 12:44:37 -0500

    Documented tools

--- src/tools.cpp
+++ src/tools.cpp
@@ -3,9 +3,12 @@
 std::vector<Word> Intersect(Word w1, Word w2) {
     std::vector<Word> updated_words;  // {w1a,w2a,w1b,w2b,...}
 
+    // If the orientation is the same or is not set, the intersection is not possible
     if (w1.orientation_ == w2.orientation_) return updated_words;
     if (w1.orientation_ == 0 || w2.orientation_ == 0) return updated_words;
 
+    // For sake of indexing consistency, define the "big" and the "small" word
+    // (aka long and short, just realized it now.)
     Word wbig = w1;
     Word wsmall = w2;
     bool swapped = false;
@@ -18,9 +21,12 @@ std::vector<Word> Intersect(Word w1, Word w2) {
 
     for (size_t i = 0; i < wbig.size(); i++) {
         for (size_t j = 0; j < wsmall.size(); j++) {
+            // If the letters are the same, intersection may be possible
             if (wbig.w_[i] == wsmall.w_[j]) {
+                // Both positions in both words must be available
                 if ((wbig.availability_[i + 1] == 0) && (wsmall.availability_[j + 1] == 0)) {
                     // The intersection is valid.
+                    // Define new words to push to the solution
                     Word w1a = wbig;
                     Word w2b = wsmall;
                     size_t i2 = i;
@@ -32,6 +38,7 @@ std::vector<Word> Intersect(Word w1, Word w2) {
                         j2 = i;
                     }
 
+                    // Update their availability
                     w1a.availability_[i2] = 1;
                     w1a.availability_[i2 + 1] = 2;
                     w1a.availability_[i2 + 2] = 1;
@@ -66,11 +73,14 @@ std::vector<Word> Intersect(Word w1, Word w2) {
 }
 
 void AddNewWord(Word w, Crossword cw, std::vector<Crossword> &outcome_crosswords) {
+    // this function cannot be used if the crossword is empty.
     if (cw.size() == 0) {
         return;
     }
 
+    // Loop over the words in the crossword, and try to intersect w with each one of them
     for (size_t i = 0; i < cw.size(); i++) {
+        // Need to make a copy of the word object, since it will be modified.
         Word wc = cw.words_[i];
 
         // Gett all updated words that could be added
@@ -83,13 +93,16 @@ void AddNewWord(Word w, Crossword cw, std::vector<Crossword> &outcome_crosswords
         // the crossword word for each possible crossword
         size_t n_extra = new_words.size() / 2;  // It is ensured to be even
 
+        // Create a new crossword with the new addition for each possible intersection
         for (size_t j = 0; j < n_extra; j++) {
             Crossword updated_cw = cw;
             updated_cw.words_[i] = new_words[2 * j + 1];
             bool fine = updated_cw.AddWord(new_words[2 * j]);
 
+            // Check if the word was succesfully added
             if (!fine) continue;
 
+            // If it was, just add it to the outcome crosswords!
             outcome_crosswords.push_back(updated_cw);
         }
     }
@@ -98,27 +111,36 @@ void AddNewWord(Word w, Crossword cw, std::vector<Crossword> &outcome_crosswords
 std::vector<Crossword> CreateCrosswords(std::vector<std::string> words) {
     std::vector<Crossword> output_crosswords;
 
+    // The strategy is to start with all possible 1-word crosswords
     for (size_t i = 0; i < words.size(); i++) {
+        // Create a horizontal and a vertical word object of the same string
         Word wh(words[i], 1);
         Word wv(words[i], 2);
 
+        // This anchor for the word is not related to the ones in crossword objects,
+        // but needs to be consistent.
         std::vector<int> wh_anchor = {int(GRID_SIZE / 2), int(GRID_SIZE / 2)};
         wh.anchor_ = wh_anchor;
         wv.anchor_ = wh_anchor;
 
+        // Create two crosswords, one for each orientation
         Crossword cw, cw2;
         cw.AddWord(wh);
         cw2.AddWord(wv);
 
+        // Add them to the crossword pool
         output_crosswords.push_back(cw);
         output_crosswords.push_back(cw2);
     }
 
+    // Then we try to add each word, vertical and horizontal, to each crossword,
+    // one after the other one.
     for (size_t i = 0; i < words.size(); i++) {
-        // orientation of first word is horizontal.
+        // Create words (vertical and horizontal)
         Word wh(words[i], 1);
         Word wv(words[i], 2);
 
+        // Try to add those words to each one of the crosswords unless the already have that word.
         for (size_t k = 0; k < output_crosswords.size(); k++) {
             if (output_crosswords[k].Contains(words[i])) continue;
             AddNewWord(wh, output_crosswords[k], output_crosswords);
@@ -126,6 +148,7 @@ std::vector<Crossword> CreateCrosswords(std::vector<std::string> words) {
             AddNewWord(wv, output_crosswords[k], output_crosswords);
         }
 
+        // Eliminate equivalent crosswords
         for (size_t k = 0; k < output_crosswords.size() - 1; k++) {
             size_t l = k + 1;
             while (l < output_crosswords.size()) {
--- src/tools.h
+++ src/tools.h
@@ -11,12 +11,35 @@
 #include "crossword.h"
 #include "constants.h"
 
+/**
+ * Returns a vector of words (w1a,w2a,w1b,w2b...) of all the possible
+ * intersections between w1 and w2. The new words will have the availability
+ * updated, and the correspondin anchor of the first word will be updated if the
+ * anchor of the second word is set.
+ * w1: Word to intersect
+ * w2: Word to which we want to intersect w1
+ **/
 std::vector<Word> Intersect(Word w1, Word w2);
 
+/**
+ * Adds a new word w to the crossword cw, and returns in outcome_crosswords
+ * all possible crosswords that are valid in which w has been added to cw.
+ * This number can get high pretty fast.
+ **/
 void AddNewWord(Word w, Crossword cw, std::vector<Crossword> &outcome_crosswords);
 
+/**
+ * Given a vector of word strings, it returns all the valid crosswords
+ * that can be generated with those words. Note that rotated crosswords
+ * are considered different crosswords.
+ **/
 std::vector<Crossword> CreateCrosswords(std::vector<std::string> words);
 
+/**
+ * Will count and print the 4-worded crosswords with 4 intersections
+ * This was the solution to the question asked in codesignal.
+ * However, I wanted to make something a bit more general and fun.
+ */
 int CountCrosswordsThatAreValid(std::vector<Crossword> cw);
 
 #endif
